a1d3dfe3e29bcff6773167e79854a69d35e5cb1b
MoeFingers <mbzuiter@gmail.com>
2024-05-31 20:09:33 -0600

    purified functions in frontend

--- frontend/src/pages/TestClient.jsx
+++ frontend/src/pages/TestClient.jsx
@@ -11,51 +11,56 @@ export default function TestClient() {
 
     const [peer, setPeer] = useState(null)
     const [peerId, setPeerId] = useState(null)
-    const[peerIdInput, setPeerIdInput] = useState('')
+    const [peerIdInput, setPeerIdInput] = useState('')
 
-    async function fetchCustom(method = 'GET', path = '/date', body = undefined) {
-        const res = await fetch(env.backend + path, {
-            method: method,
-            headers: method == 'POST' ? {'Content-Type': 'application/json'} : {},
-            body: JSON.stringify(body)
-        })
-        const data = await res.json()
-        return data
-    }
-    const getAllOffers = () => fetchCustom('GET', '/offer')
-    const postOffer = (offer) => fetchCustom('POST', '/offer', offer)
-    const deleteAllOffers = () => fetchCustom('DELETE', '/offer')
+    const [chatInput, setChatInput] = useState('')
+    const [visualLog, setVisualLog] = useState(['log start'])
 
 
-    async function generateOffer(){
-        const rtcConfig = {
-          iceServers: [
-            {
-              urls: "stun:stun.1.google.com:19302",
-            },
-          ],
-        };
-
-        try {
-            // construct peer connection
-            const pc = new RTCPeerConnection(rtcConfig);
-
-            // create offer
-            const offer = await pc.createOffer({
-                iceRestart: true,
-            });
-
-            // set local description
-            await pc.setLocalDescription(offer);
+    // async function fetchCustom(method = 'GET', path = '/date', body = undefined) {
+    //     const res = await fetch(env.backend + path, {
+    //         method: method,
+    //         headers: method == 'POST' ? {'Content-Type': 'application/json'} : {},
+    //         body: JSON.stringify(body)
+    //     })
+    //     const data = await res.json()
+    //     return data
+    // }
+    // const getAllOffers = () => fetchCustom('GET', '/offer')
+    // const postOffer = (offer) => fetchCustom('POST', '/offer', offer)
+    // const deleteAllOffers = () => fetchCustom('DELETE', '/offer')
 
 
-            return offer
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // async function generateOffer(){
+    //     const rtcConfig = {
+    //       iceServers: [
+    //         {
+    //           urls: "stun:stun.1.google.com:19302",
+    //         },
+    //       ],
+    //     };
 
-    async function registerWithPeerServer(){
+    //     try {
+    //         // construct peer connection
+    //         const pc = new RTCPeerConnection(rtcConfig);
+
+    //         // create offer
+    //         const offer = await pc.createOffer({
+    //             iceRestart: true,
+    //         });
+
+    //         // set local description
+    //         await pc.setLocalDescription(offer);
+
+
+    //         return offer
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
+    async function constructAndRegisterPeer(){
+        // IMPORTANT: we can set a custom id by saying Peer(id, options) // https://peerjs.com/docs/
         let peer = new Peer(env.clientPeerSettings)
 
         peer.on('open', (id) => {
@@ -68,24 +73,26 @@ export default function TestClient() {
         peer.on('connection', (connection) => {
             console.log('connection')
             connection.on('data', (data) => {
-                console.log(data)
+                console.log("received data: " + data)
             })
         })
         peer.on('close', () => {
             console.log('peer closed')
         })
 
-        setPeer(peer)
+        return peer
     }
-    async function connectToPeer(peerId){
-        console.log('attempting to connect to peer ' + peerId)
-        let connection = peer.connect(peerId)
-        connection.on('data', (data) => {
+    async function connectToPeer(currentPeer, remotePeerId){
+        console.log('attempting to connect to peer ' + remotePeerId)
+        let remoteConnection = currentPeer.connect(remotePeerId)
+        remoteConnection.on('data', (data) => {
             console.log("received data: " + data)
         })
-        connection.on('open', () => {
-            console.log('connection open')
+        remoteConnection.on('open', () => {
+            console.log('remoteConnection open')
+            remoteConnection.send(`CONNEGGTED - notification from ${peerId}`)
         })
+        return remoteConnection
     }
 
 
@@ -100,12 +107,17 @@ export default function TestClient() {
 
             <button onClick={async (event)=>{setDevResponse(await getAllOffers())}}>get all offers</button> */}
 
-            <button onClick={async (event)=>{registerWithPeerServer()}} >register with peerserver</button>
+            <button onClick={async (event)=>{setPeer(await constructAndRegisterPeer())}} >register with peerserver</button>
             {peer && <>
-                <input type="text" onChange={(event)=>{setPeerIdInput(event.target.value)}} value={peerIdInput}/><button onClick={async (event)=>{connectToPeer(peerIdInput)}}> connect to peer</button>
+                <input type="text" onChange={(event)=>{setPeerIdInput(event.target.value)}} value={peerIdInput}/><button onClick={async (event)=>{connectToPeer(peer, peerIdInput)}}> connect to peer</button>
             </>}
-            <div>my id: {peerId}</div>
-            
+            <div>my id: </div>
+            <div>{peerId}</div>
+            {peer && <>
+                <input type="text" onChange={(event)=>{setChatInput(event.target.value)}} value={chatInput}/><button onClick={async (event)=>{peer.send(chatInput)}}>send chat</button>
+            </>}
+            <hr />
+            <div className="visual-log">{visualLog.map((item, index) => <div key={index}>{item}</div>)}</div>
 
 
             <div>{devResponse && JSON.stringify(devResponse, null, 2)}</div>
